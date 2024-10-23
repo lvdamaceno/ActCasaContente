92b7c02f314a25ce9220daf8cf0520124fa14ed1
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-10-23 15:31:26 -0300

    Numnota check

--- ActCasaContente/src/br/com/sankhya/actcasacontente/ActAlterarNsuLista.java
+++ ActCasaContente/src/br/com/sankhya/actcasacontente/ActAlterarNsuLista.java
@@ -1,5 +1,7 @@
 package br.com.sankhya.actcasacontente;
 
+import java.math.BigDecimal;
+
 import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
 import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 import br.com.sankhya.extensions.actionbutton.QueryExecutor;
@@ -13,19 +15,27 @@ public class ActAlterarNsuLista implements AcaoRotinaJava {
 
 		Registro[] linhas = ctx.getLinhas();
 
-		String nsu = (String) ctx.getParam("NSU");
-		String autorizacao = (String) ctx.getParam("AUTH");
+		String paramNsu = (String) ctx.getParam("NSU");
+		String paramAutorizacao = (String) ctx.getParam("AUTH");
+
+		Integer paramNumnota = (Integer) ctx.getParam("NUMNOTA");
+		BigDecimal paramNumnotaBigDecimal = new BigDecimal(paramNumnota);
 
 		QueryExecutor query = ctx.getQuery();
 
 		for (Registro linha : linhas) {
-			query.setParam("NUFIN", linha.getCampo("NUFIN"));
-			query.setParam("NSU", nsu);
-			query.setParam("AUTH", autorizacao);
-			query.update("UPDATE TGFTEF SET AUTORIZACAO = {AUTH}, NUMNSU = {NSU} WHERE NUFIN = {NUFIN}");
+			BigDecimal numnota = (BigDecimal) linha.getCampo("NUMNOTA");
 
+			if (paramNumnotaBigDecimal.compareTo(numnota) == 0) {
+				query.setParam("NUFIN", linha.getCampo("NUFIN"));
+				query.setParam("NSU", paramNsu);
+				query.setParam("AUTH", paramAutorizacao);
+				query.update("UPDATE TGFTEF SET AUTORIZACAO = {AUTH}, NUMNSU = {NSU} WHERE NUFIN = {NUFIN}");
+
+			} else {
+				ctx.mostraErro("Mais de uma nota selecionada ou Numero da Nota informado não encontrado.");
+			}
 		}
-
 		ctx.setMensagemRetorno("Nsu/Autorização alterado com sucesso!");
 
 	}
